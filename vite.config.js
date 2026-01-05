86e40b0f9ea63ff136bd986d0d3d8d1900b15d62
Abdi Deresa <abdishegute@gmail.com>
2026-01-04 16:11:17 -0800

    added vite configuration file

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  // Your configuration options go here
+  plugins:[
+    tailwindcss(),
+  ]
+})
