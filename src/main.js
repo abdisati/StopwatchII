4ce39fa295c608445522d60aa24d145440618d3e
Abdi Deresa <abdishegute@gmail.com>
2026-01-05 19:04:15 -0800

    added event listeners

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ const startBtn=document.getElementById("btn1");
 const stopBtn=document.getElementById("btn2");
 const resetBtn=document.getElementById("btn3");
 
+
+//add event listener to button elements
+startBtn.addEventListener('click',start);
+stopBtn.addEventListener('click',stop);
+resetBtn.addEventListener('click',reset);
+
 //globally keep track of the startTime, elapsed time and interval id
 const startTime=0;
 const elapsedTime=0;
