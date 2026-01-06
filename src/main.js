d9a914d1196340d28bc67aec08ce4e9281b7fbf3
Abdi Deresa <abdishegute@gmail.com>
2026-01-05 19:56:07 -0800

    Finalized

--- src/main.js
+++ src/main.js
@@ -10,16 +10,16 @@ stopBtn.addEventListener('click',stop);
 resetBtn.addEventListener('click',reset);
 
 //globally keep track of the startTime, elapsed time and interval id
-const startTime=0;
-const elapsedTime=0;
-const timeId=null;
+let startTime=0;
+let elapsedTime=0;
+let timeId=null;
 
 //start function
 function start(){
     //check if interval is not null
     if(timeId!==null) return;
     startTime=Date.now()-elapsedTime;
-    setInterval(()=>{
+    timeId=setInterval(()=>{
         elapsedTime=Date.now()-startTime;
         updateDisplay(elapsedTime);
     },100);
@@ -35,4 +35,28 @@ function reset(){
     stop();
     elapsedTime=0;
     updateDisplay(elapsedTime);
+}
+
+function updateDisplay(ms){
+    const ftime=formatTime(ms);
+
+    displayEl.textContent=`${ftime.hour}:${ftime.minutes}:${ftime.seconds}`;
+}
+
+function formatTime(ms){
+    const totalSeconds=Math.floor(ms/1000);
+    const hrs=Math.floor(totalSeconds/3600);
+    const mn=Math.floor((totalSeconds%3600)/60);
+    const scds=totalSeconds%60;
+
+    //convert it to strings
+    const Hour=String(hrs).padStart(2,"0");
+    const Minutes=String(mn).padStart(2,"0");
+    const Seconds=String(scds).padStart(2,"0");
+
+    return {
+        hour:Hour,
+        minutes:Minutes,
+        seconds:Seconds
+    };
 }
